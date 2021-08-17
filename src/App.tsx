11623171bf769127ffb72df5159124363712019c
Jacob <jdungan77@gmail.com>
2021-08-17 13:07:03 -0400

    added foods list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
-export function App(){
-    return <h1>Pantry Manager</h1>;
 
+
+type Food = {
+    name: string;
+    quantity: number;
+};
+
+const foods: Food[] = [
+    {name: "Carrot", quantity: 1},
+    {name: "Potato", quantity: 2}
+];
+
+export function App(){
+
+
+    function renderFoods(){
+        return foods.map((food) => <li>{food.name}</li>)
+    }
+    //empty tag uses react.fragment - required for the parent element
+    return (
+    
+    <>
+        <h1>Pantry Manager</h1>
+        <ul>
+            { renderFoods()}
+        </ul>
+        
+    </>
+    );
 }
