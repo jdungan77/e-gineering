1c7b0fe31a7750acf9e483d6e628c412099b5c02
Jacob <jdungan77@gmail.com>
2021-08-17 12:17:02 -0400

    initial file commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import {render} from 'react-dom';
+import { App } from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const headStyle = {color: "blue", marginBottom: 10};
+function Heading(props: any) {
+        return <h1 className="head" style={headStyle}>
+            {props.children}
+            </h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+//Our first react component
+// HMTL vs JSX
+// inline styles are strings
+//
+
+
+//This is "JSX"
+render(<App />, document.getElementById("root"));
