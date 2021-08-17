86849d609b9f6c5652b90d4f141c87e55436c4b4
Jacob <jdungan77@gmail.com>
2021-08-17 13:41:23 -0400

    commit food table

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,44 @@
-
-
-type Food = {
-    name: string;
-    quantity: number;
+type food = {
+  name: string;
+  quantity: number;
+  minQuantity: number;
+  type: string;
 };
 
-const foods: Food[] = [
-    {name: "Carrot", quantity: 1},
-    {name: "Potato", quantity: 2}
+// Exercise 2: Add properties: reorderPoint (number), type (string) then display all in a table.
+const foods: food[] = [
+  { name: "Carrot", quantity: 1, minQuantity: 3, type: "Vegetable" },
+  { name: "Potato", quantity: 2, minQuantity: 2, type: "Vegetable" },
 ];
 
-export function App(){
-
-
-    function renderFoods(){
-        return foods.map((food) => <li>{food.name}</li>)
-    }
-    //empty tag uses react.fragment - required for the parent element
-    return (
-    
+export function App() {
+  function renderFoods() {
+    return;
+  }
+  //empty tag uses react.fragment - required for the parent element
+  return (
     <>
-        <h1>Pantry Manager</h1>
-        <ul>
-            { renderFoods()}
-        </ul>
-        
+      <h1>Pantry Manager</h1>
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Min Quantity</th>
+          <th>Type</th>
+        </thead>
+        {/* Exercise1: Display quantity next to food with a dash in between*/}
+
+        <tbody>
+          {foods.map((food) => (
+            <tr key={food.name}>
+              <td>{food.name}</td>
+              <td>{food.quantity}</td>
+              <td>{food.minQuantity}</td>
+              <td>{food.type}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-    );
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {render} from 'react-dom';
+import { render } from "react-dom";
 import { App } from "./App";
 
-const headStyle = {color: "blue", marginBottom: 10};
+const headStyle = { color: "blue", marginBottom: 10 };
 function Heading(props: any) {
-        return <h1 className="head" style={headStyle}>
-            {props.children}
-            </h1>
-
+  return (
+    <h1 className="head" style={headStyle}>
+      {props.children}
+    </h1>
+  );
 }
 //Our first react component
 // HMTL vs JSX
 // inline styles are strings
-//
-
+// in JSX comments are like this: {*/ comments like this */}
 
 //This is "JSX"
 render(<App />, document.getElementById("root"));
